9099ea53598a23acb5ca6e6121ae1551d32522f6
Shivam Agarwal <shivamagarwal36029@gmail.com>
2024-10-18 21:50:11 +0530

    updated technician service

--- technician-service/config/env.go
+++ technician-service/config/env.go
@@ -20,7 +20,7 @@ func initConfig() Config {
 	return Config{
 		Port:       getEnv("PORT", "5432"),
 		DBUser:     getEnv("DB_USER", "postgres"),
-		DBPassword: getEnv("DB_PASSWORD", "Shivam29@"),
+		DBPassword: getEnv("DB_PASSWORD", ""),
 		DBHost:     getEnv("DB_HOST", "localhost"),
 		DBName:     getEnv("DB_NAME", "mechanix"),
 	}
